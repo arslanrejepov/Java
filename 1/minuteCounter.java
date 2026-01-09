f60213ddf6d2ee162c2ef5cd0e087a9ae6284d04
Arslan Rejebov <arslanrejepov174@gmail.com>
2026-01-09 20:10:52 +0800

    Java programs #1

--- 1/minuteCounter.java
+++ 1/minuteCounter.java
@@ -4,9 +4,9 @@ public class minuteCounter {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         System.out.print("Enter the minutes:");
-        int num=input.nextInt();
-        int days = num / (24 * 60);
-        int years = num / (365 * 24 * 60);
+        long num=input.nextInt();
+        long days = num / (24 * 60);
+        long years = num / (365 * 24 * 60);
         System.out.println("Days: " + days);
         System.out.println("Years: " + years);
     }
